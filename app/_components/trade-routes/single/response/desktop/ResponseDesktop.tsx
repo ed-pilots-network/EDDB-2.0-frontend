ab53b0f5261945ba5d8d9cae764fe2f9d936fc93
aslink87 <adam.slinkman@gmail.com>
2024-06-26 16:29:31 -0400

    add filtering feat for results

--- app/_components/trade-routes/single/response/desktop/ResponseDesktop.tsx
+++ app/_components/trade-routes/single/response/desktop/ResponseDesktop.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { VStack } from '@chakra-ui/react';
+import { Button, HStack, Text, VStack } from '@chakra-ui/react';
 import GetColor from '@/app/_hooks/colorSelector';
 import ResponseLegend from '../Legend';
 import ResponseBody from './BodyDesktop';
@@ -12,28 +12,51 @@ const SingleTradeResponseDesktop = ({
 }: {
   results: FormResponseProps[];
   cargoCapacity: number;
-}) => (
-  <VStack
-    width="100%"
-    marginX="auto"
-    opacity={0.9}
-    borderWidth="2px"
-    borderRadius="9px"
-    borderColor={GetColor('border')}
-    padding="1rem"
-  >
-    <ResponseLegend />
-    {results
-      .sort((a: FormResponseProps, b: FormResponseProps) => b.profit - a.profit)
-      .map((result, index) => (
-        <ResponseBody
-          key={index}
-          index={index}
-          data={result}
-          cargoCapacity={cargoCapacity}
-        />
-      ))}
-  </VStack>
-);
+}) => {
+  const [filterValue, setFilterValue] = useState<'profit' | 'routeDistance'>(
+    'profit',
+  );
+  return (
+    <VStack
+      width="100%"
+      marginX="auto"
+      opacity={0.9}
+      borderWidth="2px"
+      borderRadius="9px"
+      borderColor={GetColor('border')}
+      padding="1rem"
+    >
+      <ResponseLegend />
+      <Text marginTop={2}>Sort by:</Text>
+      <HStack>
+        <Button
+          onClick={() => setFilterValue('profit')}
+          variant={filterValue !== 'profit' ? 'colorless' : 'outline'}
+        >
+          Profit
+        </Button>
+        <Button
+          onClick={() => setFilterValue('routeDistance')}
+          variant={filterValue !== 'routeDistance' ? 'colorless' : 'outline'}
+        >
+          Distance
+        </Button>
+      </HStack>
+      {results
+        .sort((a: FormResponseProps, b: FormResponseProps) => {
+          if (filterValue === 'profit') return b[filterValue] - a[filterValue];
+          return a[filterValue] - b[filterValue];
+        })
+        .map((result, index) => (
+          <ResponseBody
+            key={index}
+            index={index}
+            data={result}
+            cargoCapacity={cargoCapacity}
+          />
+        ))}
+    </VStack>
+  );
+};
 
 export default SingleTradeResponseDesktop;
